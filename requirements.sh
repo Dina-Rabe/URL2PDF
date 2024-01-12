5b21590267f064a1803f2e50ecd55a5094ab28fc
Dina Rabenarimanitra <dina@allo-win.net>
2024-01-12 13:57:55 +0300

    Final version

---
+++ requirements.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+################################################################################
+# Title: Requirements installation for URL2PDF
+# Description: This script will ease the task for all reauirements installation needed for this project. This was written for debian user, you can customize it depends on your Linux distribution
+# Author: Dina Rabenarimanitra
+# Date: January 12, 2024
+# Version: 1.0
+################################################################################
+
+pip install requests
+pip install BeautifulSoup
+pip install reportlab
+apt install python3-shutil
